5c0517960fdcacc5fd36205c2935172e801a56fa
KatonaMihaly <katona0607@gmail.com>
2025-04-01 17:05:56 +0200

    Added fifth rule

--- IsValidPassword.py
+++ IsValidPassword.py
@@ -1,19 +1,46 @@
-def is_valid_password(s):
-    lenght = True if len(s) >= 6 else False
-    lowercase = any(char.islower() for char in s)
-    uppercase = any(char.isupper() for char in s)
-    number = any(char.isdigit() for char in s)
-    special = any(not char.isalnum() for char in s)
-    
-    return lenght and lowercase and uppercase and number and special
+# RULESET ----------------------------------------------
+import re
 
-input_str = input("Enter a password: ")
-if is_valid_password(input_str):
-    print("Valid password.")
-else:
-    print("Invalid password.")
-    print("Must be at least 6 characters long.")
-    print("Must contain at least one number.")
-    print("Must contain at least one uppercase letter.")
-    print("Must contain at least one special character.")
-    print("Must contain at least one lowercase letter.")
+
+def is_valid_length(s):
+    return True if len(s) >= 6 else False
+
+def is_valid_lowercase(s):
+    return any(char.islower() for char in s)
+
+def is_valid_uppercase(s):
+    return any(char.isupper() for char in s)
+
+def is_valid_numeric(s):
+    return any(char.isdigit() for char in s)
+
+def is_valid_special(s):
+    return any(not char.isalnum() for char in s)
+
+def is_valid_sum(s):
+    return sum([int(char) for char in s if char.isdigit()]) == 25
+
+# def is_valid_rule(s):
+#     return True
+
+# MAIN --------------------------------------------------
+while True:
+    input_str = input("Enter a password:")
+    if not is_valid_length(input_str):
+        print("Must be at least 6 characters long.")
+    elif not is_valid_lowercase(input_str):
+        print("Must contain at least one lowercase letter.")
+    elif not is_valid_uppercase(input_str):
+        print("Must contain at least one uppercase letter.")
+    elif not is_valid_numeric(input_str):
+        print("Must contain at least one number.")
+    elif not is_valid_special(input_str):
+        print("Must contain at least one special character.")
+    elif not is_valid_sum(input_str):
+        print("The digits in the password must add up to 25.")
+    # elif not is_valid_rule(input_str):
+    #     print("Must contain ...")
+
+    else:
+        print("You win!")
+        break
